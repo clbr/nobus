12f9f6bae5538a4d738c5f6b5a19c678047a5daf
Lauri Kasanen <cand@gmx.com>
2012-05-02 15:55:49 +0300

    Moar

--- stubs.c
+++ stubs.c
@@ -136,110 +136,170 @@ dbus_bool_t dbus_connection_read_write (DBusConnection *connection,
 	return TRUE;
 }
 
-DBusMessage* dbus_connection_borrow_message (DBusConnection *connection);
+DBusMessage* dbus_connection_borrow_message (DBusConnection *connection) {
+	return NULL;
+}
 
-DBusMessage* dbus_connection_pop_message (DBusConnection *connection);
+DBusMessage* dbus_connection_pop_message (DBusConnection *connection) {
+	return NULL;
+}
 
-DBusDispatchStatus dbus_connection_get_dispatch_status (DBusConnection *connection);
+DBusDispatchStatus dbus_connection_get_dispatch_status (DBusConnection *connection) {
+	return DBUS_DISPATCH_COMPLETE;
+}
 
-DBusDispatchStatus dbus_connection_dispatch (DBusConnection *connection);
+DBusDispatchStatus dbus_connection_dispatch (DBusConnection *connection) {
+	return DBUS_DISPATCH_COMPLETE;
+}
 
-dbus_bool_t dbus_connection_has_messages_to_send (DBusConnection *connection);
+dbus_bool_t dbus_connection_has_messages_to_send (DBusConnection *connection) {
+	return FALSE;
+}
 
 dbus_bool_t dbus_connection_send (DBusConnection *connection,
  DBusMessage *message,
- dbus_uint32_t *client_serial);
+ dbus_uint32_t *client_serial) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_send_with_reply (DBusConnection *connection,
  DBusMessage *message,
  DBusPendingCall **pending_return,
- int timeout_milliseconds);
+ int timeout_milliseconds) {
+	return TRUE;
+}
 
 DBusMessage * dbus_connection_send_with_reply_and_block (DBusConnection *connection,
  DBusMessage *message,
  int timeout_milliseconds,
- DBusError *error);
+ DBusError *error) {
+	return NULL;
+}
 
 dbus_bool_t dbus_connection_set_watch_functions (DBusConnection *connection,
  DBusAddWatchFunction add_function,
  DBusRemoveWatchFunction remove_function,
  DBusWatchToggledFunction toggled_function,
  void *data,
- DBusFreeFunction free_data_function);
+ DBusFreeFunction free_data_function) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_set_timeout_functions (DBusConnection *connection,
  DBusAddTimeoutFunction add_function,
  DBusRemoveTimeoutFunction remove_function,
  DBusTimeoutToggledFunction toggled_function,
  void *data,
- DBusFreeFunction free_data_function);
+ DBusFreeFunction free_data_function) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_get_unix_user (DBusConnection *connection,
- unsigned long *uid);
+ unsigned long *uid) {
+	return FALSE;
+}
 
 dbus_bool_t dbus_connection_get_unix_process_id (DBusConnection *connection,
- unsigned long *pid);
+ unsigned long *pid) {
+	return FALSE;
+}
 
 dbus_bool_t dbus_connection_get_adt_audit_session_data (DBusConnection *connection,
  void **data,
- dbus_int32_t *data_size);
+ dbus_int32_t *data_size) {
+	return FALSE;
+}
 
 dbus_bool_t dbus_connection_get_windows_user (DBusConnection *connection,
- char **windows_sid_p);
+ char **windows_sid_p) {
+	return FALSE;
+}
 
 dbus_bool_t dbus_connection_add_filter (DBusConnection *connection,
  DBusHandleMessageFunction function,
  void *user_data,
- DBusFreeFunction free_data_function);
+ DBusFreeFunction free_data_function) {
+	return TRUE;
+}
 
-dbus_bool_t dbus_connection_allocate_data_slot (dbus_int32_t *slot_p);
+dbus_bool_t dbus_connection_allocate_data_slot (dbus_int32_t *slot_p) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_set_data (DBusConnection *connection,
  dbus_int32_t slot,
  void *data,
- DBusFreeFunction free_data_func);
+ DBusFreeFunction free_data_func) {
+	return TRUE;
+}
 
 void* dbus_connection_get_data (DBusConnection *connection,
- dbus_int32_t slot);
+ dbus_int32_t slot) {
+	return NULL;
+}
 
-long dbus_connection_get_max_message_size (DBusConnection *connection);
+long dbus_connection_get_max_message_size (DBusConnection *connection) {
+	return 10;
+}
 
-long dbus_connection_get_max_received_size (DBusConnection *connection);
+long dbus_connection_get_max_received_size (DBusConnection *connection) {
+	return 10;
+}
 
-long dbus_connection_get_max_message_unix_fds (DBusConnection *connection);
+long dbus_connection_get_max_message_unix_fds (DBusConnection *connection) {
+	return 10;
+}
 
-long dbus_connection_get_max_received_unix_fds(DBusConnection *connection);
+long dbus_connection_get_max_received_unix_fds(DBusConnection *connection) {
+	return 10;
+}
 
-long dbus_connection_get_outgoing_size (DBusConnection *connection);
+long dbus_connection_get_outgoing_size (DBusConnection *connection) {
+	return 0;
+}
 
-long dbus_connection_get_outgoing_unix_fds (DBusConnection *connection);
+long dbus_connection_get_outgoing_unix_fds (DBusConnection *connection) {
+	return 0;
+}
 
-DBusPreallocatedSend* dbus_connection_preallocate_send (DBusConnection *connection);
+DBusPreallocatedSend* dbus_connection_preallocate_send (DBusConnection *connection) {
+	return NULL;
+}
 
 dbus_bool_t dbus_connection_try_register_object_path (DBusConnection *connection,
  const char *path,
  const DBusObjectPathVTable *vtable,
  void *user_data,
- DBusError *error);
+ DBusError *error) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_register_object_path (DBusConnection *connection,
  const char *path,
  const DBusObjectPathVTable *vtable,
- void *user_data);
+ void *user_data) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_try_register_fallback (DBusConnection *connection,
  const char *path,
  const DBusObjectPathVTable *vtable,
  void *user_data,
- DBusError *error);
+ DBusError *error) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_register_fallback (DBusConnection *connection,
  const char *path,
  const DBusObjectPathVTable *vtable,
- void *user_data);
+ void *user_data) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_unregister_object_path (DBusConnection *connection,
- const char *path);
+ const char *path) {
+	return TRUE;
+}
 
 dbus_bool_t dbus_connection_get_object_path_data (DBusConnection *connection,
  const char *path,
